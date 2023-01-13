829339b67b6982e98b2a4ab71cf1707394652873
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2023-01-13 13:02:33 -0500

    run fmt

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use motorx_core::Server;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     motorx::setup_tracing();
 
-    Server::new(motorx::config_from_args()?).await.map_err(|e| e.into())
+    Server::new(motorx::config_from_args()?)
+        .await
+        .map_err(|e| e.into())
 }
-
-
