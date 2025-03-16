7c8437393da8c5687865547c33114f4f35a0e04e
tsar-boomba <itg.2048@gmail.com>
2025-03-15 20:48:49 -0700

    Fix not handling uppercased upgrade header

--- motorx-core/src/handle/mod.rs
+++ motorx-core/src/handle/mod.rs
@@ -98,7 +98,7 @@ async fn handle_match(
     //   - "upgrade" is not empty
     let connection_header = req.headers().get(CONNECTION);
     let upgrade_header = req.headers().get(UPGRADE);
-    let upgrading = connection_header.is_some_and(|v| v.as_bytes() == b"upgrade")
+    let upgrading = connection_header.is_some_and(|v| v.to_str().is_ok_and(|v| v.to_ascii_lowercase() == "upgrade"))
         && upgrade_header.is_some_and(|v| !v.is_empty());
 
     if upgrading {
